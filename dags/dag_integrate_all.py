aa70a4f75cb75cbce3315a9d23c5c9369ab5c220
Charis Chakim <m.charischakim@gmail.com>
2024-01-17 15:49:29 +0700

    update-depencencies-dag

---
+++ dags/dag_integrate_all.py
@@ -0,0 +1,80 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+import sys
+sys.path.insert(0,"/root/airflow/dags/")
+import product_categories as pc
+import products as p
+import customers as c
+import cities as ci
+import provinces as prov
+import orders as o
+import order_details as od
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+}
+
+dag = DAG(
+    'ELT_dag',
+    default_args=default_args,
+    description='ELT DAG to run many task',
+    schedule_interval=None,
+)
+
+start_operator = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+ingest_product_categories= PythonOperator(
+    task_id='ingest_product_categories',
+    python_callable=pc.main,  
+    dag=dag,
+)
+
+ingest_products= PythonOperator(
+    task_id='ingest_products',
+    python_callable=p.main,  
+    dag=dag,
+)
+
+ingest_customers= PythonOperator(
+    task_id='ingest_customers',
+    python_callable=c.main,  
+    dag=dag,
+)
+
+ingest_cities= PythonOperator(
+    task_id='ingest_cities',
+    python_callable=ci.main,  
+    dag=dag,
+)
+
+ingest_provinces= PythonOperator(
+    task_id='ingest_provinces',
+    python_callable=prov.main,  
+    dag=dag,
+)
+
+ingest_orders= PythonOperator(
+    task_id='ingest_orders',
+    python_callable=o.main,  
+    dag=dag,
+)
+
+ingest_order_details= PythonOperator(
+    task_id='ingest_order_details',
+    python_callable=od.main,  
+    dag=dag,
+)
+
+end_operator = DummyOperator(
+    task_id='end_execution',
+    dag=dag,
+)
+
+start_operator >> ingest_product_categories>> ingest_products >> ingest_customers >> ingest_provinces >> ingest_cities >> ingest_orders >> ingest_order_details >> end_operator
