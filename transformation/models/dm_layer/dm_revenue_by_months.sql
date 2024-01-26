b97b9782df2c4cbfa36d1094a4b7e0aa13b7cddb
Charis Chakim <132967245+CharisChakim@users.noreply.github.com>
2024-01-26 10:57:52 +0700

    update

--- transformation/models/dm_layer/dm_revenue_by_months.sql
+++ transformation/models/dm_layer/dm_revenue_by_months.sql
@@ -1,5 +1,5 @@
 SELECT
-    EXTRACT(MONTH FROM CAST(order_date AS TIMESTAMP)) AS month,
+    EXTRACT(MONTH FROM CAST(order_date AS TIMESTAMP)) AS month, --extract mengambil bulannya saja, date trunc di ambil di awal tanggal bulan tsb
     SUM(unit_sales) AS total_revenue
 FROM
     {{ ref('fct_transactions') }}
